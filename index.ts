ffdb8f558ffa2a66743a78a92c5ef7db7293b37d
Amir Maghami <Maghami.A1987@gmail.com>
2024-02-03 16:22:40 +0330

    fix: type

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
-export * from "./src/utils";
+// export * from "./src/utils";
 export * from "./src/element";
 export * from "./src/mount";
-export * from "./src/render";
-export * from "./src/router";
+// export * from "./src/render";
+// export * from "./src/router";
 export * from "./src/types";
