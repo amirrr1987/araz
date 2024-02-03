83e1ff1b7d8cb7ed58ece44843393537e03a45a5
Amir Maghami <Maghami.A1987@gmail.com>
2024-02-03 16:19:08 +0330

    fix: type

--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,6 @@
 export interface Mount {
   $node: Element;
-  $target: HTMLDivElement;
+  $target: HTMLElement;
 }
 
 export interface Attrs {
@@ -16,13 +16,18 @@ export interface Attrs {
     | string[]
     | undefined;
 }
-export type Children = VNode | string | VNode[] | string[];
-
+export type Children = string | string[] | Element | Element[];
+export type Tag = string;
 export interface VNode {
-  $tag: HTMLTags;
-  $attrs?: Attrs;
-  $children?: (VNode | string)[];
+  tag: Tag;
+  attrs: Attrs;
+  children: Children;
 }
+export type ElementFunction = (
+  $tag: Tag,
+  $attrs?: Attrs,
+  $children?: Children
+) => Element;
 
 export interface Route {
   path: string;
@@ -39,113 +44,3 @@ export type ReactiveObject<T> = {
 export type Reactive<T> = {
   [K in keyof T]: T[K];
 };
-export type HTMLTags =
-  | "a"
-  | "abbr"
-  | "address"
-  | "area"
-  | "article"
-  | "aside"
-  | "audio"
-  | "b"
-  | "base"
-  | "bdi"
-  | "bdo"
-  | "blockquote"
-  | "body"
-  | "br"
-  | "button"
-  | "canvas"
-  | "caption"
-  | "cite"
-  | "code"
-  | "col"
-  | "colgroup"
-  | "data"
-  | "datalist"
-  | "dd"
-  | "del"
-  | "details"
-  | "dfn"
-  | "dialog"
-  | "div"
-  | "dl"
-  | "dt"
-  | "em"
-  | "embed"
-  | "fieldset"
-  | "figcaption"
-  | "figure"
-  | "footer"
-  | "form"
-  | "h1"
-  | "h2"
-  | "h3"
-  | "h4"
-  | "h5"
-  | "h6"
-  | "head"
-  | "header"
-  | "hgroup"
-  | "hr"
-  | "html"
-  | "i"
-  | "iframe"
-  | "img"
-  | "input"
-  | "ins"
-  | "kbd"
-  | "label"
-  | "legend"
-  | "li"
-  | "link"
-  | "main"
-  | "map"
-  | "mark"
-  | "meta"
-  | "meter"
-  | "nav"
-  | "noscript"
-  | "object"
-  | "ol"
-  | "optgroup"
-  | "option"
-  | "output"
-  | "p"
-  | "param"
-  | "picture"
-  | "pre"
-  | "progress"
-  | "q"
-  | "rp"
-  | "rt"
-  | "ruby"
-  | "s"
-  | "samp"
-  | "section"
-  | "select"
-  | "small"
-  | "source"
-  | "span"
-  | "strong"
-  | "style"
-  | "sub"
-  | "summary"
-  | "sup"
-  | "table"
-  | "tbody"
-  | "td"
-  | "template"
-  | "textarea"
-  | "tfoot"
-  | "th"
-  | "thead"
-  | "time"
-  | "title"
-  | "tr"
-  | "track"
-  | "u"
-  | "ul"
-  | "var"
-  | "video"
-  | "wbr";
